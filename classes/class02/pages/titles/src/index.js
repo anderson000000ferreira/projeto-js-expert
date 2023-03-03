ac0ccf9d55da6cf7e24c8d5f30197368b1055322
Anderson Ferreira <andersonciamicro@hotmail.com>
2023-03-02 21:08:55 -0300

    adicionando arquivos do projeto

---
+++ classes/class02/pages/titles/src/index.js
@@ -0,0 +1,4 @@
+import cardsFactory from "./factories/cardsFactory.js"
+import HandGestureFactory from "./factories/handGestureFactory.js"
+await cardsFactory.initialize()
+await HandGestureFactory.initialize()
